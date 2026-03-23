8234c141e657d123ea96442069c8c382b0e553fc
25b0916 <25b0916@iitb.ac.in>
2026-03-23 14:01:05 +0530

    Added basic user input to main.sh

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Welcome to Mini Game Hub"
+
+read -p "Enter Player 1 username: " user1
+read -p "Enter Player 2 username: " user2
+
+echo "Player 1: $user1"
+echo "Player 2: $user2"
