0ad5846e966f02475e073b582b2ae1e7f1aaf3b6
25b0916 <25b0916@iitb.ac.in>
2026-03-28 16:12:39 +0530

    Added user registration and login with hashing and validation handling most of the edge cases

--- main.sh
+++ main.sh
@@ -1,52 +1,142 @@
 #!/bin/bash
 
+#File to store username and hashed passwords
 USER_FILE="users.tsv"
+
 echo "Welcome to Mini Game Hub"
+
+#---------------------------------------------------------------------------------
 # Function to hash password
+# Purpose: Takes a plaintext password and returns its SHA256 hash
+#---------------------------------------------------------------------------------
+
 hash_password(){
 	echo -n "$1" | sha256sum | cut -d ' ' -f1 	
 }
+
+#-----------------------------------------------------------------------------------
 #Function to register a new user
+# Purpose: Registers a new user with unique username and confirmed password
+#-----------------------------------------------------------------------------------
+
 register_user(){
-	read -p "Enter a username:" username
-	read -s -p "Enter a password:" password
-	hpass=$(hash_password "$password")
-	echo -e "$username\t$hpass" >> "$USER_FILE"	
+	#Loop until unique password is entered
+	while true; do
+		read -p "Enter a username:" username
+
+		#checks for non empty username
+		if [ -z "$username" ]; then
+			echo "Username cannot be empty" >&2
+			continue
+		fi
+		
+		#Removes extra blank spaces from the input
+		username=$(echo "$username" | xargs)
+
+		if grep -q "^$username\t" "$USER_FILE"; then
+			echo "Username already exists. Try another username" >&2
+			continue
+		else
+			break
+		fi
+	done
+
+	#loop until the password and confirm passwords match
+	while true; do
+		read -s -p "Enter a password: " password1
+		printf "\n"
+
+		#Enforces minimum password length requirement of 8 characters
+		if [ ${#password1} -lt 8 ]; then
+			echo "Password must be atleast 8 characters long" >&2
+			continue
+		fi
+		read -s -p "Confirm password:" password2
+		printf "\n"
+		
+		#Checks if the passwords match
+		if [ "$password1" = "$password2" ]; then
+			hpass=$(hash_password "$password1")
+			echo -e "$username\t$hpass" >> "$USER_FILE"
+			echo "Registration successful!" >&2
+			reguser="$username"
+
+			return
+		else
+			echo "Passwords do not match. Please try again." >&2
+			continue
+		fi
+		break
+	done
+	return
 }
+
+#------------------------------------------------------------------------------------------------------
 #Function to authenticate existing users
+#Purpose: Authenticates a existing user and allows registration if user not found.
+#------------------------------------------------------------------------------------------------------
+
 authenticate_user(){
+#Loop until valid login or registration
 while true; do
 	read -p "Enter a username: " username
-	#Checks if user is existing or not
+
+	#Ensures username input is not empty
+	if [ -z "$username" ]; then
+		echo "Username cannot be empty" >&2
+		continue
+	fi
+
+	#removes extra spaces
+	username=$( echo "$username" | xargs )
+
+	#Checks if user is exists
 	if grep -q "^$username\t" "$USER_FILE"; then
+
+		#password verification loop
 		while true; do
 			read -s -p "Enter password:" password
-			pass=$(hash_password "$password")
-			if grep -q "^$username[[:space:]]$pass$" "$USER_FILE"; then
-				echo "Login successful!"
-				echo "$username"
-				return
-			else 
-				echo "Incorrect password. Try again."
-			fi
-		done
-	#Ask if username wants to register
-	else
-		read -p "Username not found. Do you want to register? (y/n): " choice
-			if [ "$choice" = "y" ]; then
-				register_user
-				echo "$username"
-				return
-			else 
-				echo "Please enter username again."
+			printf "\n"
+			if [ ${#password} -lt 8 ]; then
+				echo "Password must be atleast 8 charcaters long." >&2
 				continue
 			fi
+			pass=$(hash_password "$password")
+			if grep -q "^$username\t$pass$" "$USER_FILE"; then
+				echo "Login successful!" >&2
+				echo "$username"
+				return
+			else 
+				echo "Incorrect password. Try again." >&2
+				continue
+			fi
+		done
+	#Prompt user to register if username not found
+	else
+		while true; do
+			read -p "Username not found. Do you want to register? (y/n): " choice
+			if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
+				register_user
+				#returns newly registered username
+				echo "$reguser"
+				return
+			elif [ "$choice" = "n" ] || [ "$choice" = "N" ]; then
+				echo "Please enter username again." >&2
+				break
+			else
+				echo "Invalid input. Please choose y or n" >&2
+				continue
+			fi
+		done
 	fi
 done
 }
-user1=$(authenticate_user)
-user2=$(authenticate_user)
 
+#Authenticates two users
+user1=$(authenticate_user | xargs)
+user2=$(authenticate_user | xargs)
+echo "DEBUG user1=[$user1]
+echo "DEBUG user2=[$user2]
 # ensures that both the users are different 
 while [ "$user1" = "$user2" ]; do
 	echo "Error: Both users cannot have the same usernames."
@@ -54,14 +144,31 @@ while [ "$user1" = "$user2" ]; do
 	while true; do
 		read -p "Which user wishes to re-login? (1/2): " choice
 		if [ "$choice" = "1" ]; then
+			old_user1="$user1"
 			user1=$(authenticate_user)
+			if [ "$user1" != "$old_user1" ]; then
+				break
+			else
+				echo "Username is already taken by other user. Please try another username."
+			fi
 		elif [ "$choice" = "2" ]; then
+			old_user2="$user2"
 			user2=$(authenticate_user)
+			if [ "$user2" != "$old_user2" ]; then
+				break
+			else
+				echo "Username is already taken by other user. Please try another username."
+			fi
 		#invalid input re-prompt
 		else 
-			echo "Invalid input. Choose 1 or 2."
+			echo "Invalid input. Choose 1 or 2." 
+			continue
 		fi
 	done
 done
+
+# NOTE:
+# Currently, this system doesn't handle the edge case where when the usernames of both users match and on relogin the users deliberately put the same username again. I still can't figure out the soution for this problem but once I actually do I will surely resolve it.
+
 # launch the game with the authenticated users
 python3 game.py "$user1" "$user2"
