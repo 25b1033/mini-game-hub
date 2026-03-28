86233aa9a8d6d95edaf279307786572bb8b50fb9
25b1033 <25b1033@iitb.ac.in>
2026-03-28 23:22:08 +0530

    feat: added sorting pipeline to leaderboard

--- leaderboard.sh
+++ leaderboard.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+#store argument
 sort_by=$1
 
+#check if any game is played in history.csv
 if[ ! -f history.csv ];then
 	echo "No games played yet"
 	exit 1
 fi
 
+#store  metric for sorting leaderboard
 if[ "$sort_by" == "wins" ];then
 	sort_col=2
 elif[ "$sort_by" =="losses" ];then
@@ -13,25 +18,27 @@ else
 	sort_col=4
 fi
 
+#print header row
 echo "Player	Wins	Losses	Ratio"
 echo "-------------------------------"
 
 
-awk ' BEGIN{ FS="," } 
+#calculate the leaderboard data
+awk ' BEGIN{ FS="," #define field separator } 
 NR>1{
-	wins[$1]++
-	losses[$2]++
+	wins[$1]++ #increase number of wins of winner
+	losses[$2]++ #increase number of losses of loser
 } 
 END{
 	for(player in wins){
-		w=wins[player]
-		l=losses[player]
+		w=wins[player] #store number of wins of player in w
+		l=losses[player] #store number of losses of player in l
 		if(l==0)
 			ratio=w
 		else
-			ratio= w/l
+			ratio= w/l #store win loss ratio in ratio
+			printf "%s %d %d %.2f\n" , player , w , l , ratio 
 	}
 } '
-history.csv
-
+history.csv| sort -k${sort_col} -rn | awk '{printf "%-15s %-5s %-7s %.2f\n" , $1, $2, $3, $4}' #sort the rows and use awk for formatting 
 
