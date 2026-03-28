d212487b02d640d7a045a11d3d1addb285f1a59a
25b1033 <25b1033@iitb.ac.in>
2026-03-29 00:01:34 +0530

    fix: resolve syntax and parsing errors in leaderboard.sh

--- leaderboard.sh
+++ leaderboard.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
 #store argument
-sort_by=$1
+isort_by=$1
 
 #check if any game is played in history.csv
-if[ ! -f history.csv ];then
+if [ ! -f history.csv ]; then
 	echo "No games played yet"
 	exit 1
 fi
 
 #store  metric for sorting leaderboard
-if[ "$sort_by" == "wins" ];then
+if [ "$sort_by" == "wins" ]; then
 	sort_col=2
-elif[ "$sort_by" =="losses" ];then
+elif [ "$sort_by" == "losses" ]; then
 	sort_col=3
 else 
 	sort_col=4
 fi
 
 #print header row
-echo "Player	Wins	Losses	Ratio"
-echo "-------------------------------"
+echo "Player          Wins  Losses  Ratio"
+echo "-----------------------------------"
 
 
 #calculate the leaderboard data
-awk ' BEGIN{ FS="," #define field separator } 
-NR>1{
-	wins[$1]++ #increase number of wins of winner
-	losses[$2]++ #increase number of losses of loser
+awk -F ',' ' 
+NR>1 {
+	wins[$1]++
+	losses[$2]++
 } 
 END{
 	for(player in wins){
-		w=wins[player] #store number of wins of player in w
-		l=losses[player] #store number of losses of player in l
+		w=wins[player] 
+		l=losses[player]
 		if(l==0)
 			ratio=w
 		else
-			ratio= w/l #store win loss ratio in ratio
+			ratio= w/l
 			printf "%s %d %d %.2f\n" , player , w , l , ratio 
 	}
-} '
-history.csv| sort -k${sort_col} -rn | awk '{printf "%-15s %-5s %-7s %.2f\n" , $1, $2, $3, $4}' #sort the rows and use awk for formatting 
+} ' history.csv| sort -k${sort_col} -rn | awk '{printf "%-15s %-5s %-7s %.2f\n" , $1, $2, $3, $4}' #sort the rows and use awk for formatting 
 
