d140ca1272c36c7ee440d63e1fde629172bbc6c0
25b1033 <25b1033@iitb.ac.in>
2026-04-07 20:34:22 +0530

    feat:added checkwin condition.fix:corrected syntax at some places

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -5,18 +5,42 @@ class TicTactoe(Gamebase):
     def __init__(self,player1,player2,):
         super().__init__(player1,player2,(10,10))
 
-    def validmove():
+    def validmove(self):
         return True
     
-    def checkmove():
-        return row<=10 and column<=10 and self.board[row][column]
+    def checkmove(self,row,column):
+        return row<=10 and column<=10 and self.board[row][column]==0
     
-    def make_move():
+    def make_move(self,row,column):
         if self.checkmove():
-            self.board.[row][col]=self.currentplayerindex + 1
+            self.board[row][column]=-2*self.currentplayerindex + 1
             return True
         return False
     
+    def check_win(self):
+        t=self.board
+        row = t[:,0:6] + t[:,1:7] + t[:,2:8] + t[:,3:9] + t[:,4:10]
+        col = t[0:6,:] + t[1:7,:] + t[2:8,:] + t[3:9,:] + t[4:10,:]
+        diag1= t[0:6,0:6] + t[1:7,1:7] + t[2:8,2:8] + t[3:9,3:9] + t[4:10,4:10]
+        diag2= t[4:10,0:6] + t[3:9,1:7] + t[2:8,2:8] + t[1:7,3:9] + t[0:6,4:10]
+
+        #check in row
+        if np.max(row)==5 or np.min(row)==-5:
+            return 1
+        #check in column
+        elif np.max(col)==5 or np.min(col)==-5:
+            return 1        
+        #check in diagonal
+        elif np.max(diag1)==5 or np.min(diag1)==-5:
+            return 1
+        #check in antidiagonal
+        elif np.max(diag2)==5 or np.min(diag2)==-5:
+            return 1
+        elif np.any(t==0):
+            return 2
+        else:
+            return 0
+
     def draw_piece():
         for r in range(10):
             for c in range(10):
@@ -25,7 +49,7 @@ class TicTactoe(Gamebase):
 
             font = pygame.font.SysFont('Arial',cellsize//2)
 
-            if self.board[r][c]==2:
+            if self.board[r][c]==-1:
                 o_txt=font.render("O",True,(0,0,0))
                 screen.blit(o_txt,(x,y))
             elif self.board[r][c]==1:
